aa4ecfab9cdde16fb1d6961ffd6265c28a6aeead
Палехов Фёдор <f.palekhov@domain.local>
2025-02-27 18:01:13 +0300

    Локаторы для классов

--- PyTest/test_meow22/pages/main_page.py
+++ PyTest/test_meow22/pages/main_page.py
@@ -1,11 +1,12 @@
 from .base_page import BasePage
-from selenium.webdriver.common.by import By
-
+from .locators import MainPageLocators
+from .login_page import LoginPage
 class MainPage(BasePage):
     def go_to_link_page(self):
-        login = self.browser.find_element(By.CSS_SELECTOR,"#login_link")
+        login = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login.click()
-    
+        # return LoginPage(browser = self.browser,url = self.browser.current_url)
+
     def should_be_login_link(self):
-        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Нет ссылки на страницу авторизации"
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Нет ссылки на страницу авторизации"
             
--- PyTest/test_meow22/test_main.py
+++ PyTest/test_meow22/test_main.py
@@ -4,11 +4,16 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 def test_guest_go_to_login_page(browser):
-    url = ("https://selenium1py.pythonanywhere.com")
+    url = ("https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
     page = MainPage(browser, url)
     page.open()
     page.should_be_login_link()
+    page.go_to_link_page()
+    login_page = LoginPage(browser,browser.current_url)
+    login_page.should_be_login_page()
+
 
 
